977bfb03c9019218ed1e086aceca6617862fcd7e
unknown <chsrobotics@chs7104s02.csd.local>
2016-03-18 16:57:49 -0700

    closer

--- data/full/gen.php
+++ data/full/gen.php
@@ -1,5 +1,18 @@
 <?php 
 
+
+	function genid()
+	{
+		$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+				$charactersLength = strlen($characters);
+				$handle = '';
+				for ($i = 0; $i < 6; $i++) {
+			   		$handle .= $characters[rand(0, $charactersLength - 1)];
+				}
+
+		return $handle;
+	}
+
 	$POS = new STDClass();
 	$POS->PLC_N = ["Nowhere of Circumstance", "Beacon Repair Zone", "In Floor Goal", "On Mountain Touching Floor", "On Mountain, Low Zone", "On Mountain, Mid Zone", "On Mountain, High Zone"];
 	$POS->PLC_P = [0, 5, 5, 5, 10, 20, 40];
@@ -55,39 +68,39 @@
 		$o->p2plcn = $POS->PLC_N[$o->p2plc];
 		$pts += $POS->PLC_N[$o->p2plc];
 
-		$o->flr = (int)$off[$pre.$dc."Floor Goal"];
+		$o->flr = (int)$off[$pre.$dc."Floor Goal"][$i];
 		$pts += $POS->FLR*$o->flr;
 
-		$o->high = (int)$off[$pre.$dc."High Goal"];
+		$o->high = (int)$off[$pre.$dc."High Goal"][$i];
 		$pts += $POS->HIGH*$o->high;
 
-		$o->mid = (int)$off[$pre.$dc."Mid Goal"];
+		$o->mid = (int)$off[$pre.$dc."Mid Goal"][$i];
 		$pts += $POS->MID*$o->mid;
 
-		$o->low = (int)$off[$pre.$dc."Low Goal"];
+		$o->low = (int)$off[$pre.$dc."Low Goal"][$i];
 		$pts += $POS->LOW*$o->low;
 
-		$o->tclm = (int)$off[$pre.$dc."Climbers in Shelter"];
+		$o->tclm = (int)$off[$pre.$dc."Climbers in Shelter"][$i];
 		$pts += $POS->TCLM*$o->tclm;
 
-		$o->zlc = (int)$off[$pre.$dc."Zip Line"];
+		$o->zlc = (int)$off[$pre.$dc."Zip Line"][$i];
 		$pts += $POS->ZLC*$o->zlc;
 
-		$o->acs = (int)$off[$pre.$eg."All Clear Signal"];
+		$o->acs = (int)$off[$pre.$eg."All Clear Signal"][$i];
 		$pts += $POS->ACS*$o->acs;
 
-		$o->hang = (int)$off[$pre.$eg."Robot on Pull Up Bar"];
+		$o->hang = (int)$off[$pre.$eg."Robot on Pull Up Bar"][$i];
 		$pts += $POS->HANG*$o->hang;
 
-		$o->minor = (int)$off[$pre."Minor Penalty Incurred"];
-		$o->major = (int)$off[$pre."Major Penalty Incurred"];
+		$o->minor = (int)$off[$pre."Minor Penalty Incurred"][$i];
+		$o->major = (int)$off[$pre."Major Penalty Incurred"][$i];
 
-		$pen += (int)$off[$pre."Minor Penalty Awarded"]*$POS->MINOR;
-		$pen += (int)$off[$pre."Major Penalty Awarded"]*$POS->MAJOR;
+		$pen += (int)$off[$pre."Minor Penalty Awarded"][$i]*$POS->MINOR;
+		$pen += (int)$off[$pre."Major Penalty Awarded"][$i]*$POS->MAJOR;
 		$pts += (int)$pen;
 
 		$o->score = $pts;
-		$o->penalties = $pen;
+		$o->penalty = $pen;
 
 		return $o;
 	}
@@ -96,10 +109,19 @@
 
 	$comps = array();
 	$prevcomp = "";
+
+	$match = 0;
 	for($i=0;$i<count($off["Date"]);$i++)
 	{
 		if($prevcomp != $off["Event Name"][$i])
 		{
+			if($prevcomp != "")
+			{
+				
+				$handle = genid();
+				file_put_contents("./cmp/".$handle.".json", json_encode($comps[$prevcomp]));
+			}
+
 			$prevcomp = $off["Event Name"][$i];
 			$comps[$prevcomp] = new STDClass();
 			$comps[$prevcomp]->date = $off["Date"][$i];
@@ -107,6 +129,9 @@
 			$comps[$prevcomp]->name = $prevcomp;
 			$comps[$prevcomp]->division = $off["Division"][$i];
 			$comps[$prevcomp]->rows = array();
+			echo ".";
+			$match = 0;
+		
 		}
 
 		if($off["Match Type"][$i] != 1) continue; // deal with elim later.
@@ -128,14 +153,17 @@
 
 		$row->redscore = $retc->score;
 		$row->redpenalty = $retc->penalty;
-		$row->bluescore = $bect->score;
-		$row->bluepenalty = $bect->penalty;
+		$row->bluescore = $betc->score;
+		$row->bluepenalty = $betc->penalty;
 
 		$row->redetc = $retc;
 		$row->blueetc = $betc;
 
+		$row->match = ++$match;
+
 		array_push($comps[$prevcomp]->rows, $row);
 	}
+
 
 	file_put_contents("full.json", json_encode($comps));
 
